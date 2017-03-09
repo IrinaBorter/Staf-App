db16197af7d5457440c992ee911e3bacaccd35a4
Iryna Barsukova <iryna_barsukova@epam.com>
2017-03-09 16:57:52 +0300

    Hello world app using Angular and TS

---
+++ public/dist/vendor.bundle.js
@@ -0,0 +1,79 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "http://localhost:4000/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 2:
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: Error: Cannot find module './node_modules/awesome-typescript-loader/dist/entry.js'\n    at Function.Module._resolveFilename (module.js:469:15)\n    at Function.Module._load (module.js:417:25)\n    at Module.require (module.js:497:17)\n    at require (internal/module.js:20:19)\n    at loadLoader (D:\\Staf-App\\node_modules\\loader-runner\\lib\\loadLoader.js:13:17)\n    at iteratePitchingLoaders (D:\\Staf-App\\node_modules\\loader-runner\\lib\\LoaderRunner.js:169:2)\n    at runLoaders (D:\\Staf-App\\node_modules\\loader-runner\\lib\\LoaderRunner.js:362:2)\n    at NormalModule.doBuild (D:\\Staf-App\\node_modules\\webpack\\lib\\NormalModule.js:129:2)\n    at NormalModule.build (D:\\Staf-App\\node_modules\\webpack\\lib\\NormalModule.js:180:15)\n    at Compilation.buildModule (D:\\Staf-App\\node_modules\\webpack\\lib\\Compilation.js:142:10)\n    at moduleFactory.create (D:\\Staf-App\\node_modules\\webpack\\lib\\Compilation.js:424:9)\n    at D:\\Staf-App\\node_modules\\webpack\\lib\\NormalModuleFactory.js:242:4\n    at D:\\Staf-App\\node_modules\\webpack\\lib\\NormalModuleFactory.js:93:13\n    at D:\\Staf-App\\node_modules\\tapable\\lib\\Tapable.js:204:11\n    at NormalModuleFactory.params.normalModuleFactory.plugin (D:\\Staf-App\\node_modules\\webpack\\lib\\CompatibilityPlugin.js:52:5)\n    at NormalModuleFactory.applyPluginsAsyncWaterfall (D:\\Staf-App\\node_modules\\tapable\\lib\\Tapable.js:208:13)\n    at onDoneResolving (D:\\Staf-App\\node_modules\\webpack\\lib\\NormalModuleFactory.js:68:11)\n    at onDoneResolving (D:\\Staf-App\\node_modules\\webpack\\lib\\NormalModuleFactory.js:189:6)\n    at _combinedTickCallback (internal/process/next_tick.js:67:7)\n    at process._tickCallback (internal/process/next_tick.js:98:9)");
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=vendor.bundle.js.map
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackNotifierPlugin = require('webpack-notifier');
+
+module.exports = {
+    entry: {
+        polyfills: './src/polyfills.ts',
+        vendor: './src/vendor.ts',
+        app: './src/main.ts',
+    },
+
+    output: {
+        path: path.join(__dirname, '/public/dist'),
+        filename: '[name].bundle.js',
+        publicPath: 'http://localhost:3000/',
+    },
+
+    resolve: {
+        extensions: ['.js', '.ts'],
+    },
+
+    devtool: 'cheap-module-source-map',
+
+    devServer: {
+        historyApiFallback: true,
+        stats: 'minimal',
+        compress: true,
+        port: 3000,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: ['awesome-typescript-loader', 'angular2-template-loader'],
+            },
+            {
+                test: /\.scss$/,
+                exclude: [path.join(__dirname, 'src/app')],
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.scss$/,
+                include: [path.join(__dirname, 'src/app')],
+                use: ["raw-loader", "sass-loader"],
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader',
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                use: 'file-loader?name=assets/[name].[hash].[ext]',
+            },
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+
+        new WebpackNotifierPlugin(),
+
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'vendor', 'polyfills'],
+        }),
+
+        new webpack.ContextReplacementPlugin(
+            /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+            path.resolve(__dirname, 'doesnotexist/')
+        ),
+    ]
+}
