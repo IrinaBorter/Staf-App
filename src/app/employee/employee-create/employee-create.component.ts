0ab7eeb0f4e8e1524b46ca2a72b25a416a878c75
IrinaBorter <borsukovairina@gmail.com>
2017-04-24 01:43:02 +0300

    Add Employee Create component

---
+++ src/app/employee/employee-create/employee-create.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+
+@Component({
+    template: require('./employee-create.component.html'),
+    styles: [require('./employee-create.component.scss')],
+    providers: [EmployeeService],
+})
+
+export class EmployeeCreateComponent implements OnInit {
+    employee: Employee;
+    mode: String;
+
+    constructor(
+        private employeeService: EmployeeService,
+        private route: ActivatedRoute,
+        private router: Router,
+    ) {}
+
+    ngOnInit() {
+        this.mode = this.identifyMode();
+
+        if (this.mode === 'edit') {
+            const id = this.route.params
+                .switchMap((params: Params) => this.employeeService.getEmployee(+params['id']))
+                .subscribe((employee: Employee) => {
+                    this.employee = employee;
+                });
+        } else {
+            this.employee = new Employee();
+        }
+    }
+
+    identifyMode() {
+        return this.route.snapshot.params['id'] ? 'edit' : 'create';
+    }
+
+    updateEmployee() {
+        this.employeeService.updateEmployee(this.employee).then(response => {
+            if (response.status === 200) {
+                this.router.navigateByUrl(`employees/${this.employee.id}`);
+            }
+        });
+    }
+
+    createEmployee() {
+        this.employeeService.createEmployee(this.employee).then(response => {
+            if (response.status === 200) {
+                this.router.navigateByUrl(`employees/${response.id}`);
+            }
+        });
+    }
+}
